ba7aa708d46773c16c3e657d14607a8019c27ccd
miaoshua <945970809@qq.com>
2016-01-04 23:50:12 +0800

    基类消息

---
+++ message.h
@@ -0,0 +1,19 @@
+/*======================================================
+    > File Name: message.h
+    > Author: MiaoShuai
+    > E-mail:  
+    > Other :  
+    > Created Time: 2016年01月04日 星期一 13时22分23秒
+ =======================================================*/
+#pragma once
+
+class Message
+{
+    public:
+        virtual ~Message(){}
+
+        virtual void foo()//由子类自己实现的接口
+        {
+        
+        }
+};
