cb01d5903d4d87b985a2cf4e93c006aff7540860
miaoshua <945970809@qq.com>
2016-01-04 23:52:57 +0800

    测试

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*======================================================
+    > File Name: main.cpp
+    > Author: MiaoShuai
+    > E-mail:  
+    > Other :  
+    > Created Time: 2016年01月04日 星期一 14时39分47秒
+ =======================================================*/
+
+#include "message1.h"
+#include "message2.h"
+#include "message.h"
+
+int main(int argc,char **argv)
+{
+  Message *p = Factory::produce("Message1");
+  Message *p1 = Factory::produce("Message2");
+  p->foo();
+  p1->foo();
+  delete p;
+  delete p1;
+
+  //auto p2 = Factory::produce_unique("Message1");
+  //p2->foo();
+  return 0;
+}
